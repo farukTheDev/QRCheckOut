1008806f59507556a1ec209d2a7cce2780241f00
Ömer Faruk <farukkisik.dev@gmail.com>
2022-01-11 21:32:10 +0300

    Init

---
+++ QRCheckOut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCheckOut
+//
+//  Created by Ömer Faruk KISIK on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
